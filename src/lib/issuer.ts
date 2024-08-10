0657b7220efe86c9639c0cd71a17705efea8f366
Orie Steele <orie@transmute.industries>
2024-08-10 17:32:45 -0500

    flatter

--- src/lib/issuer.ts
+++ src/lib/issuer.ts
@@ -5,9 +5,17 @@ import digester from "./digester"
 import salter from "./salter"
 import JWS from "./JWS"
 
-import { RequestIssuer,  Salter,  Digester, Signer, IssuedCompactSdJwt } from "../types"
+import moment from "moment"
 
-import { _issue } from "./_issue"
+import { RequestIssuer, IssuedCompactSdJwt } from "../types"
+
+import {
+  DIGEST_ALG_KEY,
+  COMBINED_serialization_FORMAT_SEPARATOR,
+} from "./constants";
+
+import { issuancePayload } from "../YAML-SD/issuancePayload";
+import { sortProtectedHeader } from './sortProtectedHeader'
 
 const issuer = (options: RequestIssuer) => {
   if (options.privateKeyJwk){
@@ -23,28 +31,41 @@ const issuer = (options: RequestIssuer) => {
     throw new Error('alg must be passed as an option or restricted via privateKeyJwk')
   }
   return {
-    issue: async ({ claimset, jwk, kid }: { claimset: string, jwk?:any, kid?: any }): Promise<IssuedCompactSdJwt> => {
+    issue: async ({ claimset, jwk, kid, iat, exp }: { claimset: string, jwk?:any, kid?: any, iat?: string, exp?: string }): Promise<IssuedCompactSdJwt> => {
       if (!options.signer){
         if (!options.privateKeyJwk){
           throw new Error("signer or privateKeyJwk required for issuance")
         }
         options.signer = await JWS.signer(options.privateKeyJwk)
       }
-      return _issue({
-        alg: options.alg as string,
+      if (!options.digester){
+        throw new Error('digester is required.')
+      }
+      const config = {
+        disclosures: {},
+        salter: options.salter,
+        digester: options.digester,
+      }
+      const issuedPayload = await issuancePayload(YAML.load(claimset), config);
+      const protectedHeader = { alg: options.alg, kid: options.kid, typ: options.typ, cty: options.cty }
+      const claims = {
         iss: options.iss,
-        kid: options.kid,
-        typ: options.typ,
-        cty: options.cty,
-        salter: options.salter as Salter,
-        digester: options.digester as Digester,
+        iat: iat || moment().unix(),
+        exp: exp || moment().add(2, 'weeks').unix(),
         cnf: jwk || kid ? {
           jwk,
           kid,
         }: undefined,
-        signer: options.signer,
-        claims: YAML.load(claimset)
-      })
+        [DIGEST_ALG_KEY]: config.digester.name,
+        ...issuedPayload
+      }
+      const issuedJwt = await options.signer.sign({
+        protectedHeader: sortProtectedHeader(protectedHeader),
+        claimset: claims,
+      });
+      const issuedSdJwt = issuedJwt + COMBINED_serialization_FORMAT_SEPARATOR + Object.keys(config.disclosures)
+      .join(COMBINED_serialization_FORMAT_SEPARATOR)
+      return issuedSdJwt as IssuedCompactSdJwt;
     }
   }
 }
